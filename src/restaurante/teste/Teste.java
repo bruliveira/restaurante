edacf7d1ef917235545c143d02075c8bdbbcc86c
viseu.bruna.oliveira <bruna.oliveira@grupoviseu.com.br>
2023-05-14 23:54:27 -0300

    update

--- src/restaurante/teste/Teste.java
+++ src/restaurante/teste/Teste.java
@@ -7,7 +7,6 @@ import restaurante.dados.Usuario;
 import restaurante.repositorio.ContaCrud;
 import restaurante.repositorio.GarcomCrud;
 import restaurante.repositorio.PedidoCrud;
-import restaurante.repositorio.ProdutoCrud;
 
 public class Teste{
     public static void main(String[] args) {
@@ -19,16 +18,41 @@ public class Teste{
         
         new Produto("Suco de laranja", 12, 5, 5, 34);
         Produto produto1 = new Produto("Suco de laranja", 12, 5, 5, 34);
+        Produto produto2 = new Produto("Suco de manga", 12, 5, 5, 34);
+        
         //Pedido
-        Pedido pedido1 = new Pedido(1, 2, 5, false);
-        //pedido1.listcodProduto.add(produto1);
+        Pedido pedido1 = new Pedido(1, 2, 0, false);
+        Pedido pedido2 = new Pedido(2, 2, 20, false);
+        //pedido1.listcodProduto.add(produto2);
 
         System.out.println("Pedidos");
         PedidoCrud pedidos = new PedidoCrud();
-        pedidos.addPedido(pedido1, produto1);
+
+        pedidos.addPedidoZero(pedido1, produto1);
+        pedidos.addPedido(pedido1, produto2);
         pedidos.listarTodosPedidos();
 
+      
+
         System.out.println("\nConta");
+        Conta conta1 = new Conta(1, 0, true);
+
+        //conta1.listCodPedido.add(pedido1);
+
+        ContaCrud contas = new ContaCrud();
+
+        contas.addContaPrimeira(conta1, pedido1);
+        contas.addConta(conta1, pedido2);
+
+        contas.listarTodasConta();
+
+        contas.finalizarConta(1);
+        contas.listarTodasConta();
+    }
+}
+/*
+//Testando conta
+ System.out.println("\nConta");
         Conta conta1 = new Conta(1, 70, true);
 
         //conta1.listCodPedido.add(pedido1);
@@ -39,10 +63,7 @@ public class Teste{
 
         contas.listarTodasConta();
 
-    }
-}
-/*
-//Testando conta
+        
         Conta conta1 = new Conta(1, 70, true);
         //conta1.listCodPedido.add(pedido1);
         Conta conta2 = new Conta(3, 10, true);
